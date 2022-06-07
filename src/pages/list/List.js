c390dcc03129c011a9b1de6d5be4d4f3d0f077d5
RoseGolden5320 <makela.adams@gmail.com>
2022-06-06 22:42:16 -0400

    setup users page

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,7 +1,23 @@
 import './List.scss';
 
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      {/* sidebar */}
+      <Sidebar />
+
+      <div className="listContainer">
+        {/* navbar */}
+        <Navbar />
+
+        {/* data table */}
+        data table
+      </div>
+    </div>
+  );
 };
 
 export default List;
